e461856dbb2723fc1b9996b213a4088488682d0f
Tinesh Roy <tinesh12.roy@gmail.com>
2022-03-05 20:34:26 +0700

    splash screen with lottie

---
+++ lib/splash_screen.dart
@@ -0,0 +1,33 @@
+import 'package:finite_automata/home.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({ Key? key }) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    navigateInto();
+  }
+
+  navigateInto()async{
+    await Future.delayed(const Duration(milliseconds: 3850), (){
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const MyHomePage(title: 'Finite Automata')));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child:  Lottie.asset('assets/book.json'),
+      ),
+    );
+  }
+}
