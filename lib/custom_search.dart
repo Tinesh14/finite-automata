ccd0febcb3e6e4814393408ed551dbfcac286fef
Tinesh Roy <tinesh12.roy@gmail.com>
2022-04-03 20:39:05 +0700

    get all data string

--- lib/custom_search.dart
+++ lib/custom_search.dart
@@ -1,8 +1,12 @@
 // ignore_for_file: non_constant_identifier_names
 
+import 'dart:convert';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class CustomSearchDelegate extends SearchDelegate {
+  final firestoreInstance = FirebaseFirestore.instance;
   @override
   List<Widget> buildActions(BuildContext context) {
     return [
@@ -27,39 +31,55 @@ class CustomSearchDelegate extends SearchDelegate {
 
   @override
   Widget buildResults(BuildContext context) {
+    if (query.length > 3) {
+      getMarker();
+      searching("THIS IS A TEST TEXT");
+    }
     return Container();
+  }
+
+  searching(String text) {
+    var temp = text;
+    var temp1 = query;
+    List<String> pat = [];
+    List<String> searchText = [];
+    for (var i in temp1.runes) {
+      searchText.add(String.fromCharCode(i));
+    }
+    for (var rune in temp.runes) {
+      pat.add(String.fromCharCode(rune));
+    }
+    search(searchText, pat);
+  }
+
+  Future<List<String>> getMarker() async {
+    var snapshot = await firestoreInstance.collection('users').get();
+    var listData = snapshot.docs.map((doc) => doc.data()).toList();
+    List<String> listString = [];
+    for (var e in listData) {
+      listString.add(e['word']);
+      // debugPrint("test datanya : ${jsonEncode(e)}, ${e['word']}");
+    }
+    return listString;
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    if(query.length > 3){
-      var temp = "AABAACAADAABAAABAA";
-     var temp1 = query;
-     List<String>pat = [];
-     List<String>searchText = [];
-     for(var i in temp1.runes){
-       searchText.add(String.fromCharCode(i));
-     }
-     for (var rune in temp.runes) {
-       pat.add(String.fromCharCode(rune));
-     }
-     search(searchText, pat);
-    }
     return Column();
   }
 
-  static  getNextState(List<String>data, int m, int state, int x){
-    if((state < m) && (x == data[state].codeUnits.elementAt(0))){
+  static getNextState(List<String> data, int m, int state, int x) {
+    if ((state < m) && (x == data[state].codeUnits.elementAt(0))) {
       return state + 1;
     }
 
-    for(var ns = state; ns > 0; ns--){
-      if(data[ns-1].codeUnits.elementAt(0) == x){
-        for(var i = 0; i < ns-1; i++){
-          if(data[i] != data[state-ns+1+i]) {
+    for (var ns = state; ns > 0; ns--) {
+      if (data[ns - 1].codeUnits.elementAt(0) == x) {
+        for (var i = 0; i < ns - 1; i++) {
+          if (data[i] != data[state - ns + 1 + i]) {
             break;
           }
-          if(i == ns-1){
+          if (i == ns - 1) {
             return ns;
           }
         }
@@ -68,28 +88,29 @@ class CustomSearchDelegate extends SearchDelegate {
     return 0;
   }
 
-  static void computeTF(List<String>data , int m, List<List<int>> tf){
-    for(var state = 0; state <= m; ++state){
-      for(var x = 0; x < 256; ++x){
+  static void computeTF(List<String> data, int m, List<List<int>> tf) {
+    for (var state = 0; state <= m; ++state) {
+      for (var x = 0; x < 256; ++x) {
         tf[state][x] = getNextState(data, m, state, x);
       }
-    } 
+    }
   }
 
-  static void search(List<String> data, List<String> data1){
+  static void search(List<String> data, List<String> data1) {
     int m = data.length;
     int n = data1.length;
 
-    var TF = List.generate(m+1, (i) => List.filled(256, 0, growable: true));
+    var TF = List.generate(m + 1, (i) => List.filled(256, 0, growable: true));
     computeTF(data, m, TF);
 
     int state = 0;
-    for(var i = 0; i < n; i++){
-      state = TF[state][data1[i].codeUnits.elementAt(0)]; 
+    for (var i = 0; i < n; i++) {
+      state = TF[state][data1[i].codeUnits.elementAt(0)];
       debugPrint("cek state value : $state, $i, $n, ${data1[i]}");
-      if(state == m){
-        debugPrint("Pattern found at index ${(i-m+1)}"); //INDEX KE 12 GK MUNCUL
-      }else{
+      if (state == m) {
+        debugPrint(
+            "Pattern found at index ${(i - m + 1)}"); //INDEX KE 12 GK MUNCUL
+      } else {
         debugPrint("$state, $m");
       }
     }
